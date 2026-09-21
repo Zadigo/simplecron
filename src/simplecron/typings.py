9dfe4be0f625caec07ecdffa683c551abda8ed57
Zadigo <pendenquejohn@gmail.com>
2026-09-21 15:45:50 +0200

    Update job function typing

--- src/simplecron/typings.py
+++ src/simplecron/typings.py
@@ -1,15 +1,16 @@
 import datetime
-from typing import TYPE_CHECKING, Awaitable, Callable, Sequence, Union
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Protocol, Sequence, Union
 
 if TYPE_CHECKING:
     from src.simplecron.base import BaseScheduler, Cancel, Job
+    from src.simplecron.context import Context
 
 
 type TypeJob = "Job"
 
-type TypeJobReturn = "Cancel" | None
+type TypeJobReturn = Cancel | None
 
-type TypeJobFunction[T = "Job", R = TypeJobReturn] = Callable[[T], R]
+# type TypeJobFunction[T = "Job", R = TypeJobReturn] = Callable[[T], R]
 
 type TypeAsyncJobFunction[T = "Job", R = TypeJobReturn] = Callable[[T], Awaitable[R]]
 
@@ -18,3 +19,10 @@ type TypeBaseScheduler = "BaseScheduler"
 type TypeEventListenerCallback = Callable[[Sequence["Job"]], None]
 
 type TypeDatetimes = Union[datetime.datetime, datetime.time, datetime.timedelta]
+
+
+class JobFunction[T: Job, R: TypeJobReturn](Protocol):
+    def __call__(self, job: T, context: Context | None = None, **kwargs: Any) -> R: ...
+
+
+type TypeJobFunction[T: Job, R: TypeJobReturn] = JobFunction[T, R]
